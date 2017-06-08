9389cd904f090a9599720117ae74de00b61aa23a
Lee Han Kyeol <lee0hankyeol@gmail.com>
2017-06-08 18:46:44 +0900

    Updated places

--- bot.js
+++ bot.js
@@ -5,42 +5,50 @@ var Bot = require('slackbots');
 
 var bot = new Bot({
   token: process.env.BOT_API_KEY,
-  name: 'bbbot',
+  name: 'bbbot'
 });
 
 var botUser = {};
 var channels = [];
 var places = [
-  '공수간',
-  '리퀴드 참프루',
+  '고운님 (제철 남도음식, 요일 백반, 7000원)',
+  '공부가 (중식)',
+  '동인동 칼국수(매운버섯칼국수)',
   '맥도날드',
   '반룡산',
   '반포식스',
   '버거킹',
   '브라운돈까스',
   '서브웨이',
+  '석기정 부대찌개',
+  '선릉 우리집만두',
+  '소백 (주꾸미 볶음, 콩나물국밥)',
+  '스모키살룬',
+  '슬로우시티 집밥',
   '선릉순대국',
   '알로이타이',
-  '알탕',
   '이화수 육개장',
-  '전집',
-  '제육쌈밥',
-  '채린',
-  '추어탕',
+  '진고래 (복국)',
+  '탄(라멘, 돈까스)',
   '평가옥',
+  '포스코 지하 김밥',
+  '포스코 지하 백반',
+  '포스코 19층 겐지야 (일식)',
+  '포스코 19층 휘닉스(중식당)',
+  '하동관',
+  '호천탕 (메밀면, 돈까스)'
 ];
 
 const _isChatMessage = function(message) {
   return message.type === 'message' && Boolean(message.text);
-}
+};
 const _isChannelConversation = function(message) {
-  return typeof message.channel === 'string' &&
-    message.channel[0] === 'C';
-}
-const _isFromBBBot = function (message) {
+  return typeof message.channel === 'string' && message.channel[0] === 'C';
+};
+const _isFromBBBot = function(message) {
   return message.user === botUser.id;
 };
-const _isMentioningMeal = function (message) {
+const _isMentioningMeal = function(message) {
   return message.text.toLowerCase().indexOf('식샤') > -1;
 };
 
@@ -54,17 +62,18 @@ bot.on('start', function() {
 });
 
 bot.on('message', function(message) {
-  if (_isChatMessage(message) &&
+  if (
+    _isChatMessage(message) &&
     _isChannelConversation(message) &&
     !_isFromBBBot(message) &&
     _isMentioningMeal(message)
   ) {
-    var channel = function(channelId) {
-      return channels.filter(function (item) {
+    var channel = (function(channelId) {
+      return channels.filter(function(item) {
         return item.id === channelId;
       })[0];
-    }(message.channel);
+    })(message.channel);
     var place = places[Math.floor(Math.random() * places.length)];
-    bot.postMessageToChannel(channel.name, message, {as_user: true});
+    bot.postMessageToChannel(channel.name, message, { as_user: true });
   }
 });
